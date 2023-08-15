4b8e91b642ec5491b688036c4d732bc3efd922f8
mohamedtarek45 <moka76615@gmail.com>
2023-08-15 14:24:08 +0300

    dd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	char x;
 	int z;
 
-	for (x = 'a'; x <= 'z' ; x++)
+	for (z =  1; z <= 10; z++)
 	{
-		for (z = 1; z <= 10 ; z++)
+		for (x = 'a'; x <= 'z' ; x++)
 		{
-			_putchar(x);
+				_putchar(x);
 		}
 	}
 	_putchar('\n');

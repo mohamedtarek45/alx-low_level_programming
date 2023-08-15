37b551047d3ccfd27a3b9f42c77d15447e1b1ba8
mohamedtarek45 <moka76615@gmail.com>
2023-08-15 15:08:15 +0300

    dd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - tests whether a character is a
+ *@c: character to test.
  * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
 int _islower(int c)

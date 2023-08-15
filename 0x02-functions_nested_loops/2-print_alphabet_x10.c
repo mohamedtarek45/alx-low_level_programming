b0e468b9258c24b954b7fce88b62d2e5f924bc03
mohamedtarek45 <moka76615@gmail.com>
2023-08-15 14:26:35 +0300

    dd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
+ * print_alphabet_x10 - prints the string "Holberton" from a character array.
+ * Return: void..
  */
 void print_alphabet_x10(void)
 {
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 
 	for (z =  1; z <= 10; z++)
 	{
-		for (x = 'a'; x <= 'z' ; x++)
+	for (x = 'a'; x <= 'z' ; x++)
 		{
 				_putchar(x);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
